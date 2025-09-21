a31696278a947d76a1e6fa80c938dc1f97ad240b
lithium1on <lithetanium@gmail.com>
2025-09-21 11:02:01 +0200

    yes

--- demo.js
+++ demo.js
@@ -5,31 +5,24 @@ import { ImGui, ImGuiImplWeb } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
     await ImGuiImplWeb.Init({ canvas: myCanvas, enableDemos: true });
 
     const ImageFileCache = {};
-    async function ImageFile(path, width = 128, height = 128) {
+    function ImageFile(path, width = 128, height = 128) {
         if (!ImageFileCache[path]) {
             const img = new Image();
             img.src = path;
-            await new Promise(resolve => img.onload = resolve);
-
-            const gl = myCanvas.getContext("webgl2");
-            const tex = gl.createTexture();
-            gl.bindTexture(gl.TEXTURE_2D, tex);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-            gl.bindTexture(gl.TEXTURE_2D, null);
-
-            ImageFileCache[path] = { tex, w: img.width, h: img.height };
+            img.onload = () => {
+                const tex = ImGuiImplWeb.CreateTextureFromImage(img);
+                ImageFileCache[path] = { tex, w: img.width, h: img.height };
+            };
+            return;
         }
 
         const data = ImageFileCache[path];
-        ImGui.Image(data.tex, [width || data.w, height || data.h]);
+        if (data && data.tex) {
+            ImGui.Image(data.tex, [width || data.w, height || data.h]);
+        }
     }
 
-    async function frame() {
+    function frame() {
         ImGuiImplWeb.BeginRender();
 
         ImGui.Begin("about");
@@ -52,7 +45,7 @@ import { ImGui, ImGuiImplWeb } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
                 globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
             }
 
-            await ImageFile("assets/img/atm.png", 256, 256);
+            ImageFile("assets/img/atm.png", 256, 256);
 
             ImGui.TreePop();
         }
