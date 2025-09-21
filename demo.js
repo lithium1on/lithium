4354e79a047c2393f2d494a8a1a79ed3509385d9
lithium1on <lithetanium@gmail.com>
2025-09-21 11:55:28 +0200

    yesssss thanks

--- demo.js
+++ demo.js
@@ -15,18 +15,22 @@ const canvas = document.querySelector("#imgui-canvas");
     function frame() {
         ImGuiImplWeb.BeginRender();
 
+        ImGui.Begin("about");
+        ImGui.Text("hi, i'm lithium.\ni like eating batteries (sarcasm)\nrelationship helper\nfrench guy");
+        ImGui.End();
+
         ImGui.Begin("projects");
         ImGui.Text("here are some pretty cool stuff i made;");
         if (ImGui.TreeNode("lithium's atm")) {
-            ImGui.Text("cool deposit game i made using ");
+            ImGui.Text("cool deposit game i made using");
             ImGui.SameLine();
             if (ImGui.TextLink("regui")) globalThis.open("https://github.com/depthso/Dear-Regui", "_blank");
 
-            ImGui.Text("game link: ");
+            ImGui.Text("game link:");
             ImGui.SameLine();
-            if (ImGui.TextLink("roblox.com/games/106912201193396")) globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
+            if (ImGui.TextLink("https://www.roblox.com/games/106912201193396")) globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
 
-            ImGui.Image(new ImTextureRef(texId), new ImVec2(img.width, img.height));
+            ImGui.Image(new ImTextureRef(texId), new ImVec2(img.width / 2, img.height / 2));
 
             ImGui.TreePop();
         }
