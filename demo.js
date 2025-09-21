600f33693a62596499ed6461afaf7fccbf263ba3
lithium1on <lithetanium@gmail.com>
2025-09-21 10:29:07 +0200

    lithium's atm

--- demo.js
+++ demo.js
@@ -6,9 +6,29 @@ import { ImGui, ImGuiImplWeb } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
 
     function frame() {
         ImGuiImplWeb.BeginRender();
+        // about
 		ImGui.Begin("about");
 		ImGui.Text("hi, i'm lithium.\ni like eating batteries (sarcasm)\nrelationship helper\nfrench guy\n\nmy time: {time}\nmy lovely weather: {weather}");
 		ImGui.End();
+        // projects
+        ImGui.Begin("projects");
+        ImGui.Text('here are some pretty cool stuff i made;\n\n')
+        if (ImGui.TreeNode("lithium's atm")) {
+            ImGui.Text("cool deposit game i made using ");
+            ImGui.SameLine();
+            if (ImGui.TextLink("regui")) {
+                globalThis.open("https://github.com/depthso/Dear-Regui", "_blank");
+            }
+
+            ImGui.Text("game link: ");
+            ImGui.SameLine();
+            if (ImGui.TextLink("roblox.com/games/106912201193396")) {
+                globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
+            }
+
+            ImGui.TreePop();
+        }
+
         ImGuiImplWeb.EndRender();
         requestAnimationFrame(frame);
     }
