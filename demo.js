af3209bdca0b3aa098876250406bffea3dd01e23
lithium1on <lithetanium@gmail.com>
2025-09-21 12:06:44 +0200

    rat

--- demo.js
+++ demo.js
@@ -5,11 +5,20 @@ const canvas = document.querySelector("#imgui-canvas");
 (async () => {
     await ImGuiImplWeb.Init({ canvas, enableDemos: false });
 
-    let texId = ImGuiImplWeb.LoadTexture(); 
-    const img = new Image();
-    img.src = "assets/img/atm.png";
-    img.onload = () => {
-        ImGuiImplWeb.LoadTexture(img, { id: texId });
+    // atm image
+    let atmTexId = ImGuiImplWeb.LoadTexture();
+    const atm = new Image();
+    atm.src = "assets/img/atm.png";
+    atm.onload = () => {
+        ImGuiImplWeb.LoadTexture(atm, { id: atmTexId });
+    };
+
+    // ratware image
+    let ratTexId = ImGuiImplWeb.LoadTexture();
+    const ratware = new Image();
+    ratware.src = "assets/img/ratware.png";
+    ratware.onload = () => {
+        ImGuiImplWeb.LoadTexture(ratware, { id: ratTexId });
     };
 
     function frame() {
@@ -32,10 +41,18 @@ const canvas = document.querySelector("#imgui-canvas");
             ImGui.SameLine();
             if (ImGui.TextLink("https://www.roblox.com/games/106912201193396")) globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
 
-            ImGui.Image(new ImTextureRef(texId), new ImVec2(img.width / 1.5, img.height / 1.5));
+            ImGui.Image(new ImTextureRef(texId), new ImVec2(atm.width / 1.3, img.height / 1.3));
 
             ImGui.TreePop();
         }
+        if (ImGui.TreeNode("ratware")) {
+            ImGui.Text("very dead project, executor that was last updated in march (worse than awp)");
+            ImGui.Image(new ImTextureRef(texId), new ImVec2(ratware.width / 1.3, img.height / 1.3));
+            ImGui.Text("ratware isnt coming back any time soon (whole server dead :broken-heart:)")
+            ImGui.TreePop();
+        }
+        ImGui.Text("\ncheck back later for more thx");
+
         ImGui.End();
 
         ImGuiImplWeb.EndRender();
