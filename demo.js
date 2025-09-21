1dce8a71ca81fdb7cab8e26b617e2714ec970d6b
lithium1on <lithetanium@gmail.com>
2025-09-21 11:06:08 +0200

    test

--- demo.js
+++ demo.js
@@ -1,57 +1,22 @@
-import { ImGui, ImGuiImplWeb } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
+import { ImGui, ImGuiImplWeb, ImTextureId } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
 
 (async () => {
     const myCanvas = document.querySelector("#imgui-canvas");
     await ImGuiImplWeb.Init({ canvas: myCanvas, enableDemos: true });
 
-    const ImageFileCache = {};
-    function ImageFile(path, width = 128, height = 128) {
-        if (!ImageFileCache[path]) {
-            const img = new Image();
-            img.src = path;
-            img.onload = () => {
-                const tex = ImGuiImplWeb.CreateTextureFromImage(img);
-                ImageFileCache[path] = { tex, w: img.width, h: img.height };
-            };
-            return;
-        }
+    const image = new Image();
+    image.src = "assets/img/atm.png";
+    await new Promise((resolve) => {
+        image.onload = resolve;
+    });
 
-        const data = ImageFileCache[path];
-        if (data && data.tex) {
-            ImGui.Image(data.tex, [width || data.w, height || data.h]);
-        }
-    }
+    const texture = await ImGuiImplWeb.CreateTextureFromImage(image);
 
     function frame() {
         ImGuiImplWeb.BeginRender();
-
-        ImGui.Begin("about");
-        ImGui.Text("hi, i'm lithium.\ni like eating batteries (sarcasm)\nrelationship helper\nfrench guy\n\nmy time: {time}\nmy lovely weather: {weather}");
+        ImGui.Begin("Image Example");
+        ImGui.Image(texture, new ImVec2(256, 256));
         ImGui.End();
-
-        ImGui.Begin("projects");
-        ImGui.Text('here are some pretty cool stuff i made;\n\n');
-
-        if (ImGui.TreeNode("lithium's atm")) {
-            ImGui.Text("cool deposit game i made using ");
-            ImGui.SameLine();
-            if (ImGui.TextLink("regui")) {
-                globalThis.open("https://github.com/depthso/Dear-Regui", "_blank");
-            }
-
-            ImGui.Text("game link: ");
-            ImGui.SameLine();
-            if (ImGui.TextLink("roblox.com/games/106912201193396")) {
-                globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
-            }
-
-            ImageFile("assets/img/atm.png", 256, 256);
-
-            ImGui.TreePop();
-        }
-
-        ImGui.End();
-
         ImGuiImplWeb.EndRender();
         requestAnimationFrame(frame);
     }
