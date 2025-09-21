8462665020cc19360cfcdd3fbe1c0afbe5e8ca36
lithium1on <lithetanium@gmail.com>
2025-09-21 11:23:52 +0200

    pls

--- demo.js
+++ demo.js
@@ -1,19 +1,23 @@
 import { ImGui, ImGuiImplWeb } from "https://esm.sh/@mori2003/jsimgui@0.8.0";
+import { createImageBitmap } from 'https://unpkg.com/@mori2003/jsimgui@0.8.0';
 
 (async () => {
     const myCanvas = document.querySelector("#imgui-canvas");
     await ImGuiImplWeb.Init({ canvas: myCanvas, enableDemos: true });
 
-    const image = await ImGui.ImageFile("assets/img/atm.png");
-    const texture = ImGui.CreateTextureFromImage(image);
+    const img = await createImageBitmap(await fetch("assets/img/atm.png").then(res => res.blob()));
+    const tex = ImGuiImplWeb.CreateTextureFromImage(img);
+    const texId = tex ? tex.id : null;
 
     function frame() {
         ImGuiImplWeb.BeginRender();
-
-        ImGui.Begin("Image Window");
-        ImGui.Image(texture);
+        ImGui.Begin("Image Example");
+        if (texId) {
+            ImGui.Image(texId, [img.width, img.height]);
+        } else {
+            ImGui.Text("Failed to load image.");
+        }
         ImGui.End();
-
         ImGuiImplWeb.EndRender();
         requestAnimationFrame(frame);
     }
