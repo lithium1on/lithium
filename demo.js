509367b72c34a22fc009ea876975b1654a0a4a76
lithium1on <lithetanium@gmail.com>
2025-09-21 12:08:04 +0200

    fixed

--- demo.js
+++ demo.js
@@ -41,13 +41,13 @@ const canvas = document.querySelector("#imgui-canvas");
             ImGui.SameLine();
             if (ImGui.TextLink("https://www.roblox.com/games/106912201193396")) globalThis.open("https://www.roblox.com/games/106912201193396/", "_blank");
 
-            ImGui.Image(new ImTextureRef(texId), new ImVec2(atm.width / 1.3, img.height / 1.3));
+            ImGui.Image(new ImTextureRef(atmTexId), new ImVec2(atm.width / 1.3, img.height / 1.3));
 
             ImGui.TreePop();
         }
         if (ImGui.TreeNode("ratware")) {
             ImGui.Text("very dead project, executor that was last updated in march (worse than awp)");
-            ImGui.Image(new ImTextureRef(texId), new ImVec2(ratware.width / 1.3, img.height / 1.3));
+            ImGui.Image(new ImTextureRef(ratTexId), new ImVec2(ratware.width / 1.3, img.height / 1.3));
             ImGui.Text("ratware isnt coming back any time soon (whole server dead :broken-heart:)")
             ImGui.TreePop();
         }
